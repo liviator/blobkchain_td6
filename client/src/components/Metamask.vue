b6edd98e3acee520c77d3c0b4f21a192db55eed3
alex-bcht <alex.bauchet95160@gmail.com>
2021-04-07 13:18:54 +0200

    4 pts

--- client/src/components/Metamask.vue
+++ client/src/components/Metamask.vue
@@ -12,34 +12,34 @@
 import Web3 from 'web3';
 export default {
   name: 'ConnectMetamask',
+//   data() {
+//       return {
+//         chainId: undefined,
+//       }
+//   },
   methods: {
     async connectMetamask() {
-
-      if (typeof window.ethereum !== 'undefined') {
-        window.web3 = new Web3(window.ethereum);
-        window.ethereum.enable();
+        if (typeof window.ethereum !== 'undefined') {
+            var chainId = parseInt(window.ethereum.chainId);
+            window.web3 = new Web3(window.ethereum);
+            window.ethereum.enable();
             window.web3.eth.getAccounts((error,result) => {
                 if(error){ console.log(error)}
                 else{
-                    var adress = result
+                    var address = result
                     window.web3.eth.getBlockNumber((error,result) => {
-                if(error){ console.log(error)}
-                else{
-                    var blocknum = result
-                    console.log(window.web3.currentProvider.)
-                     this.$store.dispatch('login',{adress,blocknum})
-                }})
-                
+                        if(error){ console.log(error)}
+                        else{
+                            var blocknum = result
+                            this.$store.dispatch('login', {address, blocknum, chainId})
+                        }
+                    })
                 }
             })
-
-        
-        
-        
-      }
-       else {
-        console.error('MetaMask is not detected!');
-      }
+        }
+        else {
+            console.error('MetaMask is not detected!');
+        }
     },
   },
 };
--- client/src/views/song.vue
+++ client/src/views/song.vue
@@ -3,28 +3,34 @@
 </template>
 
 <script>
-
- 
-var Web3 = require('web3')
-
-
+import Web3 from 'web3'
+const abi = require('../../../contract/build/SongForACity.json').abi
+const address = '0x004a84209a0021b8ff182ffd8bb874c53f65e90e'
 
 export default ({
     setup() {
     },
     async mounted(){
-    let web3js =window.web3 
-     if (web3js){
-        web3js= new Web3(web3js.currentProvider)
-        window.ethereum.enable()
-    }
-        console.log(web3js)
-        let pute = await web3js.eth.getBlockNumber()
-        alert(pute)
-        
-        
-        web3js.eth.net.GetId(console.log)
+        const web3 = new Web3(Web3.currentProvider || "http://localhost:7545")
+        Web3.givenProvider.enable()
 
+        var myContract = new web3.eth.Contract(abi, address);
+        const nametoken = await myContract.methods.name().call()
+        console.log(nametoken)
+        // let web3 = window.web3 
+        // if (web3){
+        //     web3 = new Web3(web3.currentProvider)
+        //     window.ethereum.enable()
+        // }
+        // web3.eth.defaultAccount = web3.eth.accounts[0];
+        // console.log(web3.eth.defaultAccount)
+        
+        // var initContract = web3.eth.contract(abi);
+        // var contract = initContract.at(address);
+
+        // contract.totalSupply().call().then(function(result){
+        //     console.log('result : ', result);
+        // });
     }
 })
 </script>
